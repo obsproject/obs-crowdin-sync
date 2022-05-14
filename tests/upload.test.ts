cefa24cc348d36a7ce0241af96012e008d3434a1
Vainock <vainock@pm.me>
2022-05-14 19:30:02 +0200

    upload: Upload new files to Crowdin

--- tests/upload.test.ts
+++ tests/upload.test.ts
@@ -1,5 +1,6 @@
 import NOCK from 'nock';
 import FSE from 'fs-extra';
+import PATH from 'path';
 import * as ACTIONS from '@actions/core';
 
 import { upload } from '../src/upload';
@@ -13,11 +14,18 @@ beforeAll(async () => {
 	if (await FSE.pathExists(rootDir)) {
 		await FSE.rm(rootDir, { recursive: true });
 	}
-	await FSE.mkdir(`${rootDir}/UI/data/locale`, { recursive: true });
+	await FSE.mkdir(rootDir, { recursive: true });
 	process.chdir(rootDir);
-	await FSE.mkdir('plugins/sndio/data/locale', { recursive: true });
-	await FSE.writeFile('UI/data/locale/en-US.ini', '\n# Comment"\nYes="Yes"\nCancel="Cancel"\n');
-	await FSE.writeFile('plugins/sndio/data/locale/en-US.ini', 'Device="Device"\nRate="Rate"\n');
+	for (const [filePath, content] of Object.entries({
+		'UI/data/locale/en-US.ini': '\n# Comment"\nYes="Yes"\nCancel="Cancel"\n',
+		'UI/frontend-plugins/some-frontend/data/locale/en-US.ini': 'MyFrontendString="Text"',
+		'plugins/sndio/data/locale/en-US.ini': 'Device="Device"\nRate="Rate"\n',
+		'plugins/my-plugin/data/locale/en-US.ini': 'MyPluginString="Text"',
+		'plugins/data/locale/en-US.ini': '123'
+	})) {
+		await FSE.mkdir(PATH.parse(filePath).dir, { recursive: true });
+		await FSE.writeFile(filePath, content);
+	}
 
 	const MAX_API_PAGE_SIZE = 500;
 	scopeMain = NOCK(`https://api.crowdin.com/api/v2/projects/${PROJECT_ID}`)
@@ -39,52 +47,60 @@ beforeAll(async () => {
 		.put('/files/29', {
 			storageId: 1
 		})
-		.reply(200, {
-			data: [
-				{
-					data: {
-						identifier: 'Yes',
-						text: 'Yes'
-					}
-				},
-				{
-					data: {
-						identifier: 'Cancel',
-						text: 'Cancel'
-					}
-				},
-				{
-					data: {
-						identifier: 'OK',
-						text: 'OK'
-					}
-				},
-				{
-					data: {
-						identifier: 'Apply',
-						text: 'Apply'
-					}
-				}
-			]
-		});
+		.reply(200)
+		.post('/files', {
+			storageId: 2,
+			directoryId: 28,
+			name: 'my-plugin.ini',
+			exportOptions: {
+				exportPattern: '/plugins/%file_name%/data/locale/%locale%.ini'
+			}
+		})
+		.reply(201)
+		.post('/files', {
+			storageId: 3,
+			directoryId: 136,
+			name: 'some-frontend.ini',
+			exportOptions: {
+				exportPattern: '/UI/frontend-plugins/%file_name%/data/locale/%locale%.ini'
+			}
+		})
+		.reply(201);
 
 	scopeStorages = NOCK('https://api.crowdin.com/api/v2/storages')
 		.post('', '\n# Comment"\nYes="Yes"\nCancel="Cancel"\n')
-		.matchHeader('Crowdin-API-FileName', 'File.ini')
 		.reply(201, {
 			data: {
 				id: 1
+			}
+		})
+		.post('', 'MyPluginString="Text"')
+		.reply(201, {
+			data: {
+				id: 2
+			}
+		})
+		.post('', 'MyFrontendString="Text"')
+		.reply(201, {
+			data: {
+				id: 3
 			}
 		});
 });
 
 it(upload.name, async () => {
-	const noticeMock = jest.spyOn(ACTIONS, 'notice').mockImplementation(() => {});
-	const errorMock = jest.spyOn(ACTIONS, 'setFailed').mockImplementation(() => {});
-	await upload('UI/data/locale/en-US.ini\nAUTHORS\nUI/adv-audio-control.cpp\nplugins/decklink/data/locale/en-US.ini');
+	const noticeMock = jest.spyOn(ACTIONS, 'notice').mockImplementation(a => {});
+	const errorMock = jest.spyOn(ACTIONS, 'error').mockImplementation(() => {});
+
+	await upload(
+		'UI/data/locale/en-US.ini\nAUTHORS\nUI/frontend-plugins/some-frontend/data/locale/en-US.ini\nplugins/my-plugin/data/locale/en-US.ini\nplugins/data/locale/en-US.ini'
+	);
+
 	expect(noticeMock).toBeCalledWith('UI/data/locale/en-US.ini updated on Crowdin.');
+	expect(noticeMock).toBeCalledWith('UI/frontend-plugins/some-frontend/data/locale/en-US.ini uploaded to Crowdin.');
+	expect(noticeMock).toBeCalledWith('plugins/my-plugin/data/locale/en-US.ini uploaded to Crowdin.');
 	expect(errorMock).toBeCalledWith(
-		"Some files couldn't be found on Crowdin and/or their export paths need to be fixed to get updated:\n plugins/decklink/data/locale/en-US.ini"
+		'plugins/data/locale/en-US.ini not uploaded to Crowdin due to its unexpected location. This may be intended.'
 	);
 });
 
