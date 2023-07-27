6de60a1fa882e9c9c47542c0bb3d5c3c6215502c
Vainock <vainock@pm.me>
2023-07-27 13:41:10 +0200

    download: Correct original author

--- src/strings.ts
+++ src/strings.ts
@@ -7,7 +7,7 @@ const strings = {
 		commitTitle: 'Update translations from Crowdin'
 	},
 	authors: {
-		header: 'Original Author: Hugh Bailey ("Jim")\n\nContributors are sorted by their amount of commits / translated words.\n\n',
+		header: 'Original Author: Lain Bailey\n\nContributors are sorted by their amount of commits / translated words.\n\n',
 		contributors: 'Contributors',
 		translators: 'Translators',
 		fileName: 'AUTHORS'
